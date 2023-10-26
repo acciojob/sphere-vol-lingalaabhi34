6bf5bd54cc6d5ff1deb83a378cbf830700cbde6f
lingalaabhi34 <122089325+lingalaabhi34@users.noreply.github.com>
2023-10-27 00:23:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,10 @@
 function volume_sphere() {
-    // Prevent the form from submitting and refreshing the page
-    event.preventDefault();
-
-    // Get the radius value from the input field
-    var radius = parseFloat(document.getElementById("radius").value);
-
-    // Check if the entered value is a valid number
-    if (isNaN(radius)) {
-        alert("Please enter a valid number for the radius.");
-        return;
-    }
-
-    // Calculate the volume of the sphere using the formula V = (4/3) * π * r^3
-    var volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
-
-    // Display the volume in the volume input field
-    document.getElementById("volume").value = volume.toFixed(2); // Rounded to 2 decimal places
-}
-
-window.onload = function () {
-    // Add an event listener to the form submit button
-    document.getElementById('MyForm').onsubmit = volume_sphere;
-};
+            var radius = document.getElementById('radius').value;
+            radius = Math.abs(radius);
+            var volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+            volume = volume.toFixed(4);
+            document.getElementById('volume').value = volume;
+            return false;
+        }
+ 
+      window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
